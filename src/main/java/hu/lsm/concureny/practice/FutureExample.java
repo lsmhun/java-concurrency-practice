ec3b071b19b0ad4af4577e428581765b07bdf6bf
Lorant Hid <lsmlogin@gmail.com>
2019-05-11 12:08:02 +0200

    java Future examples

---
+++ src/main/java/hu/lsm/concureny/practice/FutureExample.java
@@ -0,0 +1,52 @@
+package hu.lsm.concureny.practice;
+
+import java.security.InvalidParameterException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureExample {
+
+    private ExecutorService executor;
+
+    public static void main(String args[]) {
+        FutureExample futureExample = new FutureExample(1);
+        Future<Long> square = futureExample.calculateSquare(10);
+        try {
+            // Blocking the main thread
+            System.out.println(square.get());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public FutureExample(int threadNumber) {
+        if (threadNumber < 1) {
+            throw new InvalidParameterException("Invalid thread number: " + threadNumber);
+        }
+        if (threadNumber == 1) {
+            executor = Executors.newSingleThreadExecutor();
+        } else {
+            executor = Executors.newFixedThreadPool(threadNumber);
+        }
+    }
+
+    public Future<Long> calculateSquare(Integer input) {
+        return executor.submit(() -> {
+            Thread.sleep(1000);
+            return input * input * 1l;
+        });
+    }
+
+    public Future<Long> calculateSquareJava7(Integer input) {
+        Callable<Long> task = new Callable<Long>() {
+            @Override
+            public Long call() throws Exception {
+                Thread.sleep(1000);
+                return input * input * 1l;
+            }
+        };
+        return executor.submit(task);
+    }
+}
---
+++ src/test/java/hu/lsm/concureny/practice/FutureExampleTest.java
@@ -0,0 +1,64 @@
+package hu.lsm.concureny.practice;
+
+import org.junit.Test;
+
+import java.util.concurrent.Future;
+
+import static org.junit.Assert.assertEquals;
+
+public class FutureExampleTest {
+
+    @Test
+    public void callFutureTest() throws InterruptedException {
+        FutureExample futureExample = new FutureExample(1);
+        Future<Long> future = futureExample.calculateSquare(10);
+
+        int checkingCounter = 0;
+        while(!future.isDone()) {
+            checkingCounter++;
+            System.out.println("#" + checkingCounter + " Calculating...");
+            Thread.sleep(400);
+        }
+        assertEquals(3, checkingCounter);
+    }
+
+    @Test
+    public void callFutureJava7StyleTest() throws InterruptedException {
+        FutureExample futureExample = new FutureExample(1);
+        Future<Long> future = futureExample.calculateSquareJava7(10);
+
+        int checkingCounter = 0;
+        while(!future.isDone()) {
+            checkingCounter++;
+            System.out.println("#" + checkingCounter + " Calculating...");
+            Thread.sleep(400);
+        }
+        assertEquals(3, checkingCounter);
+    }
+
+    @Test
+    public void checkFutureWithOneThread() throws InterruptedException  {
+        // Checking with 1 executor
+        checkFutureWithOneOrMoreThreads(1, 5);
+    }
+
+    @Test
+    public void checkFutureParallel() throws InterruptedException  {
+        // Checking with 2 executors
+        checkFutureWithOneOrMoreThreads(2, 3);
+    }
+
+    private void checkFutureWithOneOrMoreThreads(int threadNumber, int expectedWaitingCycle) throws InterruptedException{
+        FutureExample futureSingleThreaded = new FutureExample(threadNumber);
+        Future<Long> future1 = futureSingleThreaded.calculateSquare(10);
+        Future<Long> future2 = futureSingleThreaded.calculateSquare(100);
+
+        int checkingCounter = 0;
+        while(!(future1.isDone() && future2.isDone())) {
+            checkingCounter++;
+            System.out.println("#" + checkingCounter + " Calculating...");
+            Thread.sleep(450);
+        }
+        assertEquals(expectedWaitingCycle, checkingCounter);
+    }
+}
