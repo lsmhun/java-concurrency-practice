33db1e63d5a84088930890420b0d7db6f461e410
lsmaster <lsmlogin@gmail.com>
2019-05-14 17:11:53 +0200

    CyclicBarrier tests and behavior presentation

---
+++ src/main/java/hu/lsm/concureny/practice/CyclicBarrierExample.java
@@ -0,0 +1,67 @@
+package hu.lsm.concureny.practice;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+*  https://www.java67.com/2012/08/10-advanced-core-java-interview.html
+* Some differences
+* - CyclicBarrier can be reset 
+* - CountDownLatch will 
+* https://javarevisited.blogspot.com/2012/07/cyclicbarrier-example-java-5-concurrency-tutorial.html
+*/
+public class CyclicBarrierExample {
+
+    //Runnable task for each thread
+    public static class Task extends Thread {
+
+        private CyclicBarrier barrier;
+        private AtomicInteger counter;
+
+        public Task(CyclicBarrier barrier, AtomicInteger counter) {
+            this.counter = counter;
+            this.barrier = barrier;
+        }
+
+        @Override
+        public void run() {
+            try {
+                int waitingMillisec = ThreadLocalRandom.current().nextInt(0, 100);
+                // simulating long running task
+                System.out.println(Thread.currentThread().getName() + " will sleep " + waitingMillisec + " ms");
+                Thread.sleep(waitingMillisec);
+                System.out.println(Thread.currentThread().getName() + " is waiting on barrier");
+                barrier.await();
+                System.out.println(Thread.currentThread().getName() + " has crossed the barrier");
+                counter.incrementAndGet();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(CyclicBarrierExample.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+            } catch (BrokenBarrierException ex) {
+                Logger.getLogger(CyclicBarrierExample.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+
+        //creating CyclicBarrier with 3 parties i.e. 3 Threads needs to call await()
+        final CyclicBarrier cb = new CyclicBarrier(3, () -> System.out.println("All parties are arrived at barrier, lets play"));
+
+        AtomicInteger counter = new AtomicInteger(0);
+        System.out.println("Counter: " + counter.intValue());
+        //starting each of thread
+        Thread t1 = new Thread(new Task(cb, counter), "Thread 1");
+        Thread t2 = new Thread(new Task(cb, counter), "Thread 2");
+        Thread t3 = new Thread(new Task(cb, counter), "Thread 3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+
+}
---
+++ src/test/java/hu/lsm/concureny/practice/CyclicBarrierExampleTest.java
@@ -0,0 +1,94 @@
+package hu.lsm.concureny.practice;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.*;
+
+public class CyclicBarrierExampleTest {
+
+    CyclicBarrierExample cyclicBarrierExample = new CyclicBarrierExample();
+
+    @Test
+    public void testCyclicBarrierThreadsWillWaitForEachOther() throws InterruptedException {
+        //creating CyclicBarrier with 3 parties i.e. 3 Threads needs to call await()
+        final CyclicBarrier cb = new CyclicBarrier(3, () -> System.out.println("All parties are arrived at barrier, lets play"));
+
+        AtomicInteger counter = new AtomicInteger(0);
+        System.out.println("Counter: " + counter.intValue());
+        List<Thread> threads = new ArrayList<>();
+        // Run 10 threads to increment the same variable
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(new CyclicBarrierExample.Task(cb, counter), "Thread #" + i);
+            thread.start();
+            threads.add(thread);
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println("Counter: " + counter.intValue());
+        assertEquals(3, counter.intValue());
+    }
+
+    @Test
+    public void testCyclicBarrierThreadsJust2OfUs() throws InterruptedException {
+        //creating CyclicBarrier with 3 parties i.e. 3 Threads needs to call await()
+        final CyclicBarrier cb = new CyclicBarrier(3, () -> System.out.println("All parties are arrived at barrier, lets play"));
+
+        AtomicInteger counter = new AtomicInteger(0);
+        List<Thread> threads = new ArrayList<>();
+        // Run 10 threads to increment the same variable
+        for (int i = 1; i <= 2; i++) {
+            Thread thread = new Thread(new CyclicBarrierExample.Task(cb, counter), "Thread #" + i);
+            thread.start();
+            threads.add(thread);
+        }
+        Thread.sleep(300);
+        System.out.println("All two threads are still waiting on barrier, but will never step forward");
+        assertEquals(3, cb.getParties());
+        assertEquals(2, cb.getNumberWaiting());
+
+        // Counter was never changed
+        assertEquals(0, counter.intValue());
+        // cleaning
+        for(Thread thread: threads){
+            thread.interrupt();
+        }
+    }
+
+    @Test
+    public void testCyclicBarrierReset() throws InterruptedException {
+        //creating CyclicBarrier with 3 parties i.e. 3 Threads needs to call await()
+        final CyclicBarrier cb = new CyclicBarrier(3, () -> System.out.println("All parties are arrived at barrier, lets play"));
+
+        AtomicInteger counter = new AtomicInteger(0);
+        List<Thread> threads = new ArrayList<>();
+        // Run 10 threads to increment the same variable
+        for (int i = 1; i <= 2; i++) {
+            Thread thread = new Thread(new CyclicBarrierExample.Task(cb, counter), "Thread #" + i);
+            thread.start();
+            threads.add(thread);
+        }
+        Thread.sleep(300);
+        assertEquals(3, cb.getParties());
+        assertEquals(2, cb.getNumberWaiting());
+        cb.reset();
+        assertEquals(3, cb.getParties());
+        assertEquals(0, cb.getNumberWaiting());
+        for (int i = 3; i <= 5; i++) {
+            Thread thread = new Thread(new CyclicBarrierExample.Task(cb, counter), "Thread #" + i);
+            thread.start();
+            threads.add(thread);
+        }
+        Thread.sleep(300);
+        System.out.println("Thread #1 and Thread #2 will drop broken barrier exception");
+        assertEquals(3, counter.intValue());
+
+    }
+
+}
